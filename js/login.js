9224caae400450929f7af7807329b398ee12d229
Rachel Scalfani <rscalfani@gmail.com>
2013-07-08 16:39:30 -0700

    used formHelper

--- js/login.js
+++ js/login.js
@@ -1,14 +1,30 @@
-$(function() {
-	$('#login').click(function() {
-		var fields = ['firstName', 'lastName', 'email', 'password', 'confirmPassword'];
-		if (fields.some(function(id) {
-			return $('#' + id).val() == ''
-		}))
-		{
-			$('#required').show();
-			return false;
-		}
-		else
-			$('#required').hide();
-	})
+define(function(require) {
+	var formHelper = require('formHelper');
+	var formHelp = formHelper.newFormHelp();
+	var validation = require('validation');
+	var validator = validation.newValidator();
+	var $ = require('jquery');
+
+	$(function() {
+		$('#login').click(function() {
+			var validationDef = {
+				email: {
+					regex: validation.regexes.email
+				},
+				'remember-me': null,
+				password: null
+			}
+			var errors = validator.validate(formHelp.getFormData($('.form-login')), validationDef);
+			if (errors)
+			{
+				console.log(errors);
+//				$('#required').show();
+				return false;
+			}
+			else
+				console.log(errors);
+//				$('#required').hide();
+			return false; // TODO remove this after testing
+		});
+	});
 });
